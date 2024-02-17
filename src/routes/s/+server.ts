1ac6a16fd98041b53ab7c45c081fa52d429f2aac
Aser <aserdipanda@gmail.com>
2024-02-17 01:18:40 +0100

    keep internationalization on redirect when aceessing the search page without two positions and redirecting to the root page

--- src/routes/s/+server.ts
+++ src/routes/s/+server.ts
@@ -1,6 +1,8 @@
 import { redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { i18n } from '$lib/i18n';
 
 export const GET: RequestHandler = async () => {
-    redirect(307, "/");
+
+    redirect(307, i18n.resolveRoute("/"));
 };
